02ab13baee7364db2a2e7b0306476ddd081c1abf
Haitham Awad <haitham2004xx@gmail.com>
2024-11-23 10:25:15 -0500

    fixed endpoint and cleaned up unit tests

--- tests/backend/test_create_user.py
+++ tests/backend/test_create_user.py
@@ -20,15 +20,15 @@ def test_valid_user():
 #test if the same email is used for 2 different users
 def test_duplicate_emails():
     response = client.post("/api/register", json={
-        "email": "user@example.com",
+        "email": "user1@example.com",
         "password": "securePassword",
         "username": "user123"
     })
 
     response_duplicate = client.post("/api/register", json={
-        "email": "user@example.com",
+        "email": "user1@example.com",
         "password": "otherSecurePassword",
         "username": "user321"
     })
-    assert response.status_code == 400
-    assert response.json() == {"detail":"Email is not unique"}
+    assert response_duplicate.status_code == 400
+    assert response_duplicate.json() == {"detail":"Email is not unique"}
