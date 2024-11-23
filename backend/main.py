197fb70962c15d2bfbe8e4ba48921ebc1c696eef
Haitham Awad <haitham2004xx@gmail.com>
2024-11-22 22:45:23 -0500

    implement user sign up endpoint (task4)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+import bcrypt
+
+#User object to save user info
+class User(BaseModel):
+    email: str
+    password: str
+    username: str
+
+#initialize set to save emails and database to store info
+email_set = set()
+database = {}
+
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/users",status_code=status.HTTP_201_CREATED)
+async def create_user_profile(user: User):
+    #if duplicate email then throw error
+    if user.email in email_set:
+        raise HTTPException(status_code=400, detail="Email is not unique")
+    email_set.add(user.email)
+
+    #encrypt password and save to database
+    encrypted_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    database[user.email] = {
+        "email":user.email,
+        "password":encrypted_password,
+        "username":user.username
+    }
+    return { "message": "User registered" }
+
---
+++ tests/backend/task4Tests.py
@@ -0,0 +1,34 @@
+
+from fastapi.testclient import TestClient
+from fastapi import FastAPI
+import pytest
+from backend.main import app
+
+
+
+client = TestClient(app)
+# Test for valid data
+def test_valid_user():
+    response = client.post("/users", json={
+        "email": "user@example.com",
+        "password": "securePassword",
+        "username": "user123"
+    })
+    assert response.status_code == 201
+    assert response.json() == {"message": "User registered"}
+
+#test if the same email is used for 2 different users
+def test_duplicate_emails():
+    response = client.post("/users", json={
+        "email": "user@example.com",
+        "password": "securePassword",
+        "username": "user123"
+    })
+
+    response_duplicate = client.post("/users", json={
+        "email": "user@example.com",
+        "password": "otherSecurePassword",
+        "username": "user321"
+    })
+    assert response.status_code == 400
+    assert response.json() == {"detail":"Email is not unique"}
