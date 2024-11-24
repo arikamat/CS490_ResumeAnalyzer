b0df56006e40e6ad57c4ed84c21e30d255ebb59c
Noah Paul <ndp@njit.edu>
2024-11-23 22:13:42 -0500

    Implemented Resume Upload Field

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,16 +1,71 @@
 import { useState } from 'react'
 import './FileUpload.css'
+import axios from 'axios';
 
-function App() {
+const FileUpload = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleChange = (e) => {
+    const selectedFile = e.target.files[0];
+    
+    if (!selectedFile) return;
+
+    if (selectedFile.type !== 'application/pdf') {
+      setError('File must be a PDF');
+      setSuccess('');
+      setFile(null);
+      return
+    }
+
+    if (selectedFile.size > 2 * 1024 * 1024) {
+      setError('File size must be under 2 MB');
+      setSuccess('');
+      setFile(null);
+      return;
+    }
+
+    setError('');
+    setFile(selectedFile);
+    setSuccess('File is ready to upload');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!file) {
+      setError('Please select a valid PDF before submitting');
+      setSuccess('');
+      return
+    }
+    
+    setSuccess('File is ready to upload');
+    setError('');
+  }
+
+
   return (
     <div className="App">
-        <form>
-          <h1>React File Upload</h1>
-          <input type="file" />
-          <button type="submit">Upload</button>
+        <form onSubmit={handleSubmit}>
+          <h1>Resume File Upload</h1>
+
+          <div className='input-container'>
+            <input 
+              type="file" 
+              onChange={handleChange}
+            />  
+            <button type="submit">Upload</button>
+          </div>
+
+          <div className='message-container'>
+            {error && <p className="error">{error}</p>}
+            {success && <p className="success">{success}</p>}
+          </div>
+
         </form>
     </div>
   );
 }
 
-export default App
+export default FileUpload
