4a0dc58609e53439274a1d4a7ec662ad8ea9382c
Ari Kamat <arijkamat@gmail.com>
2024-11-23 19:58:57 -0500

    Formatting

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 
-#User object to save user info
+
+# User object to save user info
 class User(BaseModel):
     email: str
     password: str
--- tests/backend/test_create_user.py
+++ tests/backend/test_create_user.py
@@ -1,34 +1,44 @@
-
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 import pytest
 from backend.main import app
 
 
-
 client = TestClient(app)
+
+
 # Test for valid data
 def test_valid_user():
-    response = client.post("/api/register", json={
-        "email": "user@example.com",
-        "password": "securePassword",
-        "username": "user123"
-    })
+    response = client.post(
+        "/api/register",
+        json={
+            "email": "user@example.com",
+            "password": "securePassword",
+            "username": "user123",
+        },
+    )
     assert response.status_code == 201
     assert response.json() == {"message": "User registered"}
 
-#test if the same email is used for 2 different users
-def test_duplicate_emails():
-    response = client.post("/api/register", json={
-        "email": "user1@example.com",
-        "password": "securePassword",
-        "username": "user123"
-    })
 
-    response_duplicate = client.post("/api/register", json={
-        "email": "user1@example.com",
-        "password": "otherSecurePassword",
-        "username": "user321"
-    })
+# test if the same email is used for 2 different users
+def test_duplicate_emails():
+    response = client.post(
+        "/api/register",
+        json={
+            "email": "user1@example.com",
+            "password": "securePassword",
+            "username": "user123",
+        },
+    )
+
+    response_duplicate = client.post(
+        "/api/register",
+        json={
+            "email": "user1@example.com",
+            "password": "otherSecurePassword",
+            "username": "user321",
+        },
+    )
     assert response_duplicate.status_code == 400
-    assert response_duplicate.json() == {"detail":"Email is not unique"}
+    assert response_duplicate.json() == {"detail": "Email is not unique"}
