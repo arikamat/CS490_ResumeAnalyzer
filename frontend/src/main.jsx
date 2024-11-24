15cc5a2ec83a19b4805520007a8cf37a7f87d144
Jeremy Kurian <jeremykurian68@gmail.com>
2024-11-24 17:19:24 -0500

    changing main back

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import Navbar from './components/Navbar.jsx';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import FileUpload from './components/FileUpload.jsx'
+import JobDescription from './components/JobDescription.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-    <App/>
-  </StrictMode>
-);
+    <FileUpload/>
+    <JobDescription/>
+  </StrictMode>,
+)
