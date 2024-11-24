a3004bf8112e531aeb1be3fab1e64885446005b2
Ari Kamat <arijkamat@gmail.com>
2024-11-23 19:38:02 -0500

    Formatting

--- backend/utils/resume.py
+++ backend/utils/resume.py
@@ -2,33 +2,35 @@ from PyPDF2 import PdfReader
 import re
 from docx import Document
 
+
 def extract_text_from_pdf(file):
     """
     Extracts and cleans text from pdf file
     Args:
        file (stream or path): Path to pdf or file stream of pdf
-    
+
     Returns:
-        str: Post processed text extracted from pdf 
+        str: Post processed text extracted from pdf
     """
     reader = PdfReader(file)
     text = ""
     for page in reader.pages:
         text += page.extract_text()
-    text = text.replace('\n\n', '<PARAGRAPH_BREAK>')
-    text = text.replace('\n', ' ')
-    text = text.replace('<PARAGRAPH_BREAK>', '\n\n')
-    text = re.sub(r'\s+', ' ', text)
+    text = text.replace("\n\n", "<PARAGRAPH_BREAK>")
+    text = text.replace("\n", " ")
+    text = text.replace("<PARAGRAPH_BREAK>", "\n\n")
+    text = re.sub(r"\s+", " ", text)
     return text.strip()
+
 
 def extract_text_from_docx(file):
     """
     Extracts and cleans text from docx file
     Args:
        file (stream or path): Path to docx or file stream of docx
-    
+
     Returns:
-        str: Post processed text extracted from docx 
+        str: Post processed text extracted from docx
     """
     doc = Document(file)
     text = []
--- tests/backend/test_utils.py
+++ tests/backend/test_utils.py
@@ -6,10 +6,16 @@ from backend.utils import extract_text_from_pdf, extract_text_from_docx
 
 TESTDIR = Path(__file__).parent / "test_files"
 
+
 def test_extract_text_from_pdf():
-    pdf_file = TESTDIR/"test_pdf_1.pdf"
+    """
+    Tests the extract_text_from_pdf function
+
+    Reads a sample PDF file and extracts the text from it. Ensures that extraction output matches the actual text in the file
+    """
+    pdf_file = TESTDIR / "test_pdf_1.pdf"
     expect_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat arcu tempus, interdum tellus non, aliquam nulla. Morbi tristique augue nec vulputate dictum. Etiam posuere lorem at lectus scelerisque feugiat. Suspendisse porta ante a elit malesuada, et viverra turpis suscipit. Maecenas fringilla nec arcu et sagittis. Vivamus dignissim ligula ac diam cursus faucibus. Suspendisse felis odio, tristique quis ultricies eget, dictum sit amet lectus. Fusce diam nisi, vulputate id laoreet ut, vehicula at lorem. Fusce malesuada turpis velit, et pulvinar neque bibendum eget. Curabitur rutrum leo ac accumsan congue. Sed porta, justo ac venenatis auctor, velit est fringilla leo, luctus fermentum ex risus a nulla. Aliquam volutpat nunc vel auctor vehicula. Nullam tempor eleifend purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut finibus scelerisque varius."
-    f = open(pdf_file,"rb")
+    f = open(pdf_file, "rb")
     data = f.read()
     f.close()
     stream = io.BytesIO(data)
@@ -17,10 +23,16 @@ def test_extract_text_from_pdf():
     actual = extract_text_from_pdf(stream)
     assert actual == expect_text
 
+
 def test_extract_text_from_docx():
-    docx_file = TESTDIR/"test_docx_1.docx"
+    """
+    Tests the extract_text_from_docx function
+
+    Reads a sample docx file and extracts the text from it. Ensures that extraction output matches the actual text in the file
+    """
+    docx_file = TESTDIR / "test_docx_1.docx"
     expect_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat arcu tempus, interdum tellus non, aliquam nulla. Morbi tristique augue nec vulputate dictum. Etiam posuere lorem at lectus scelerisque feugiat. Suspendisse porta ante a elit malesuada, et viverra turpis suscipit. Maecenas fringilla nec arcu et sagittis. Vivamus dignissim ligula ac diam cursus faucibus. Suspendisse felis odio, tristique quis ultricies eget, dictum sit amet lectus. Fusce diam nisi, vulputate id laoreet ut, vehicula at lorem. Fusce malesuada turpis velit, et pulvinar neque bibendum eget. Curabitur rutrum leo ac accumsan congue. Sed porta, justo ac venenatis auctor, velit est fringilla leo, luctus fermentum ex risus a nulla. Aliquam volutpat nunc vel auctor vehicula. Nullam tempor eleifend purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut finibus scelerisque varius."
-    f = open(docx_file,"rb")
+    f = open(docx_file, "rb")
     data = f.read()
     f.close()
     stream = io.BytesIO(data)
