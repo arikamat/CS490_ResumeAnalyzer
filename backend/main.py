dfbcb30c6c624308d1da791e3542878a532fdbb7
Ari Kamat <arijkamat@gmail.com>
2024-11-24 00:58:06 -0500

    Add router to main

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from backend.routers import UserRouter  # import the user router
+from backend.routers import UserRouter
 from backend.routers import ResumeUploadRouter
-from backend.routers import LoginRouter  # import the user router
-
+from backend.routers import LoginRouter
+from backend.routers import JobDescriptionRouter
 app = FastAPI()
 
 # Include the router with a prefix (optional)
 app.include_router(UserRouter)
 app.include_router(ResumeUploadRouter)
 app.include_router(LoginRouter)
+app.include_router(JobDescriptionRouter)
