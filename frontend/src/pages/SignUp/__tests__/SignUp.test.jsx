1041f043c3e88e4729e16fc2341994df8b79b9ac
Ari Kamat <arijkamat@gmail.com>
2024-11-25 00:21:57 -0500

    Add loader tests for all pages

--- frontend/src/pages/SignUp/__tests__/SignUp.test.jsx
+++ frontend/src/pages/SignUp/__tests__/SignUp.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor  } from '@testing-library/react';
+import { render, screen, waitFor,fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import SignUp from '../SignUp';
@@ -25,14 +25,14 @@ describe('SignUp Component', () => {
   //needs to be reviewed 
   it('show text if passwords do not match', async () => {
     render(<SignUp />);
-    const user = userEvent.setup(); 
+    const user = userEvent.setup();
     //use awaits to make sure jest does not proceed due to promise 
     await user.type(screen.getByPlaceholderText('Email'), 'jck44@example.com');
     await user.type(screen.getByPlaceholderText('Username'), 'jeremy');
     await user.type(screen.getByPlaceholderText('Password'), 'safwanbad');
     await user.type(screen.getByPlaceholderText('Confirm Password'), 'wrongpass');
     await user.click(screen.getByRole('button', { name: /sign up/i }));
-    
+
     expect(await screen.findByText('Passwords do not match')).toBeInTheDocument();
     //? correct when "Signup failed email is not unique"
   });
@@ -41,7 +41,7 @@ describe('SignUp Component', () => {
     //mock a response 
     axios.post.mockResolvedValueOnce({
       status: 201,
-      data: { message: 'User registered' }, 
+      data: { message: 'User registered' },
     });
 
     render(<SignUp />);
@@ -67,9 +67,9 @@ describe('SignUp Component', () => {
   it('shows error text if signup fails due to 400 status code', async () => {
     axios.post.mockResolvedValueOnce({
       status: 400,
-      data: { message: 'Email is not unique' }, 
+      data: { message: 'Email is not unique' },
     });
-  
+
     render(<SignUp />);
     const user = userEvent.setup();
     await user.type(screen.getByPlaceholderText('Email'), 'jck44@example.com');
@@ -77,17 +77,47 @@ describe('SignUp Component', () => {
     await user.type(screen.getByPlaceholderText('Password'), 'safwanbad');
     await user.type(screen.getByPlaceholderText('Confirm Password'), 'safwanbad');
     await user.click(screen.getByRole('button', { name: /sign up/i }));
- 
+
     expect(await screen.findByText('Signup failed email is not unique')).toBeInTheDocument();
-  
+
     // Check that axios.post was called with the correct parameters
     expect(axios.post).toHaveBeenCalledWith('http://127.0.0.1:8000/api/register', {
       email: 'jck44@example.com',
       username: 'jeremy',
       password: 'safwanbad',
     });
-  
+
     // Check that axios.post was called only once
     expect(axios.post).toHaveBeenCalledTimes(1);
-  });  
+  });
+  it('displays loading component while the signup request is in progress', async () => {
+    //set timeout for entire test 
+    jest.setTimeout(10000);
+
+
+    axios.post.mockImplementation(() =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            status: 201,
+            data: { message: 'User registered' }
+          }); //make it sucess
+        }, 1000); // give one second delay
+      })
+    );
+
+    render(<SignUp />);
+    const user = userEvent.setup();
+
+    await user.type(screen.getByPlaceholderText('Email'), 'jck44@example.com');
+    await user.type(screen.getByPlaceholderText('Username'), 'jeremy');
+    await user.type(screen.getByPlaceholderText('Password'), 'safwanbad');
+    await user.type(screen.getByPlaceholderText('Confirm Password'), 'safwanbad');
+    fireEvent.click(screen.getByRole('button', { name: /sign up/i })); //trigger with fireEvent so no delay i dont want full process
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument(); // find loading to appear immedietely 
+
+    await waitFor(() => expect(screen.queryByText('Loading...')).not.toBeInTheDocument(), { timeout: 1500 }); //WAIT for loading to stop after succeess
+    expect(screen.getByText('Signup completed')).toBeInTheDocument(); //find end message
+  });
 });
