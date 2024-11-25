3242319e220da6f0332d2abd1bf1987a98e4e60f
Ari Kamat <arijkamat@gmail.com>
2024-11-24 20:06:03 -0500

    Add loading on login and signup

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactLoading from 'react-loading'
+const Loading = () => {
+    return (
+        <div>
+            <ReactLoading type="spin" color="#32a0a8" height={50} width={50}/>
+        </div>
+    )
+}
+    
+
+export default Loading;
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../../assets/Global.css';
+import Loading from '../../components/Loading'
 
 // Handles functionality of login page with email, username, password, confirmpassword
 const SignUp = () => {
@@ -9,11 +10,13 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [notification, setNotification] = useState('');
+  const [loading, setLoading] = useState(false);
 
   // Function that handles button click and sends the SignUp data to server if valid
   const onButtonClick = async (e) => {
     e.preventDefault();
-    
+    setLoading(true);
+
     if (password !== confirmPassword) {
       setNotification("Passwords do not match");
       return;
@@ -25,7 +28,7 @@ const SignUp = () => {
         username,
         password,
       });
-    
+
       if (response.status == 201) {
         setNotification("Signup completed");
       } else {
@@ -33,71 +36,75 @@ const SignUp = () => {
       }
     } catch (error) {
       setNotification(`Signup failed email is not unique`);
+    } finally{
+      setLoading(false);
     }
   };
 
   // Format of the page
   return (
-    <div className={'mainContainer'}>
-      <div className={'titleContainer'}>
-        <div>Sign Up</div>
-      </div>
-      <br />
-      <form onSubmit={onButtonClick}>
-        <div className={'inputContainer'}>
-          <input
-            type="email"
-            value={email}
-            placeholder="Email"
-            onChange={(ev) => setEmail(ev.target.value)}
-            className={'inputBox'}
-            required
-          />
+    <>
+      {loading ? (<Loading/>) : (<div className={'mainContainer'}>
+        <div className={'titleContainer'}>
+          <div>Sign Up</div>
         </div>
         <br />
-        <div className={'inputContainer'}>
-          <input
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(ev) => setUsername(ev.target.value)}
-            className={'inputBox'}
-            required
-          />
-        </div>
-        <br />
-        <div className={'inputContainer'}>
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(ev) => setPassword(ev.target.value)}
-            className={'inputBox'}
-            required
-          />
-        </div>
-        <br />
-        <div className={'inputContainer'}>
-          <input
-            type="password"
-            value={confirmPassword}
-            placeholder="Confirm Password"
-            onChange={(ev) => setConfirmPassword(ev.target.value)}
-            className={'inputBox'}
-            required
-          />
-        </div>
-        <br />
-        <div className={'inputContainer'}>
-          <input
-            className={'inputButton'}
-            type="submit"
-            value="Sign Up"
-          />
-        </div>
-      </form>
-      <p>{notification}</p>
-    </div>
+        <form onSubmit={onButtonClick}>
+          <div className={'inputContainer'}>
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={(ev) => setEmail(ev.target.value)}
+              className={'inputBox'}
+              required
+            />
+          </div>
+          <br />
+          <div className={'inputContainer'}>
+            <input
+              type="text"
+              value={username}
+              placeholder="Username"
+              onChange={(ev) => setUsername(ev.target.value)}
+              className={'inputBox'}
+              required
+            />
+          </div>
+          <br />
+          <div className={'inputContainer'}>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(ev) => setPassword(ev.target.value)}
+              className={'inputBox'}
+              required
+            />
+          </div>
+          <br />
+          <div className={'inputContainer'}>
+            <input
+              type="password"
+              value={confirmPassword}
+              placeholder="Confirm Password"
+              onChange={(ev) => setConfirmPassword(ev.target.value)}
+              className={'inputBox'}
+              required
+            />
+          </div>
+          <br />
+          <div className={'inputContainer'}>
+            <input
+              className={'inputButton'}
+              type="submit"
+              value="Sign Up"
+            />
+          </div>
+        </form>
+        <p>{notification}</p>
+      </div>)}
+    </>
   );
 };
 
