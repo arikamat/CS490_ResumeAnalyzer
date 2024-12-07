ff5a85ed8dfe6f80688f22226731aaa6a566711c
Ari Kamat <arijkamat@gmail.com>
2024-12-07 10:41:27 -0500

    Add missing keywords to output

--- backend/routers/user_input.py
+++ backend/routers/user_input.py
@@ -94,5 +94,5 @@ async def accept_user_input(user_input: UserInput):
     ai_score = response_json["fit_score"]
     score, missing = calculate_fit_score(user_input)
     response_json["fit_score"] = (response_json["fit_score"] + score*100)/2
-    print(ai_score, score, missing)
+    response_json["missing_keywords"] = missing
     return response_json
