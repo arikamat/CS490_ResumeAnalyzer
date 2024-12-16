b05e6fb70a4ea629407fe4456030b84a183ab6fe
Ari Kamat <arijkamat@gmail.com>
2024-12-16 14:06:32 -0500

    fix playwright issues

--- frontend/jest.config.jsx
+++ frontend/jest.config.jsx
@@ -1,9 +1,9 @@
-module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    testEnvironment: 'jest-environment-jsdom',
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/(?!axios)'], 
-  };
-  
+// module.exports = {
+//   transform: {
+//     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+//   },
+//   testEnvironment: 'jest-environment-jsdom',
+//   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+//   transformIgnorePatterns: ['<rootDir>/node_modules/(?!axios)'],
+//   testPathIgnorePatterns: ['<rootDir>/playwright_tests/'], 
+// };
